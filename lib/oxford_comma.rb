7dc90d339ae7f63d8e5ff93c5aa3c7641ae6970b
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-20 18:33:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,7 @@ elsif array.length == 2
   return array.join(" and ")
 else array.length >= 3
   last_array_item = "and #{array[-1]}"
+  array.pop
   array.push(last_array_item)
   return array.join(", ")
 end
