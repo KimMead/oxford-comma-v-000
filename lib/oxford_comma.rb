8322b3697bca52d25c7e2d99f71fd99a22777ff8
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-20 18:28:09 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
 if array.length == 1
   return "#{array[0]}"
-else array.length == 2
+elsif array.length == 2
   return array.join(" and ")
+else array.length == 3
+  return array.join(" , ")
 end
 end
